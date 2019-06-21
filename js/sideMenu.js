953926b4e7d255985efe936eeb90c6ba2f5541f4
nyukke415 <nyukke415@gmail.com>
2019-06-21 22:18:36 +0900

    サイドメニュー自動生成機能を実装．

---
+++ js/sideMenu.js
@@ -0,0 +1,26 @@
+/* last updated:<2019/06/21/Fri 22:13:37 from:gale-ThinkPad-T480s> */
+
+function sideMenu() {
+  var sideMenu = $("#sideMenu")
+  var internalLink = `
+<div class="sideh">Internal Link</div>
+<a href="index.html" class="button noSel sidec">Home</a>
+`
+  var jump = `
+<div class="sideh">Jump</div>
+<a href="#top" class="button noSel sidec">TOP</a>
+`
+  $('h1').each(function(){
+    var text = this.innerHTML
+    jump += `<a href="#`+text+`" class="button noSel sidec">`
+      +text+`</a>`
+    $(this).attr("id", text);
+  });
+
+  sideMenu.html(internalLink + jump)
+  $('html, body').stop().animate({scrollTop:targetTop}, targetTop = OFFSET );
+}
+
+$(function() {
+  sideMenu();
+});
