a36f2a3f2e8b11579c071f8548d43acfd4589afa
nyukke415 <nyukke415@gmail.com>
2019-07-01 01:12:07 +0900

    サイドバーのナビゲーション機能を追加．

--- js/calcAge.js
+++ js/calcAge.js
@@ -1,4 +1,4 @@
-/* last updated:<2019/06/30/Sun 23:33:33 from:gale-ThinkPad-T480s> */
+/* last updated:<2019/06/30/Sun 23:48:41 from:gale-ThinkPad-T480s> */
 
 function calcAge() {
   const birthday = new Date(1996, 4-1, 15);
@@ -6,7 +6,6 @@ function calcAge() {
   var age = today.getFullYear() - birthday.getFullYear();
   var age_m = today.getMonth() - birthday.getMonth();
   var age_d = today.getDate() - birthday.getDate();
-  console.log(age, age_m, age_d);
   if(age_d < 0) {
     age_d = -1 + new Date(today.getFullYear(), today.getMonth(), 0).getDate();
   }
--- js/sideMenu.js
+++ js/sideMenu.js
@@ -1,4 +1,4 @@
-/* last updated:<2019/06/21/Fri 23:40:58 from:gale-ThinkPad-T480s> */
+/* last updated:<2019/07/01/Mon 01:09:05 from:gale-ThinkPad-T480s> */
 
 function sideMenu() {
   var sideMenu = $("#sideMenu")
@@ -8,11 +8,11 @@ function sideMenu() {
 `
   var jump = `
 <div class="sideh">Jump</div>
-<a href="#top" class="button noSel sidec">TOP</a>
+<a href="#top" class="button noSel sidec jump active">TOP</a>
 `
   $('h1').each(function(){
     var text = this.innerHTML
-    jump += `<a href="#`+text+`" class="button noSel sidec">`
+    jump += `<a href="#`+text+`" class="button noSel sidec jump">`
       +text+`</a>`
     $(this).attr("id", text);
   });
@@ -22,4 +22,31 @@ function sideMenu() {
 
 $(function() {
   sideMenu();
+  highlightJumpList();
 });
+
+
+// 現在のスクロール位置に対応した jumpList を強調
+function highlightJumpList() {
+  var list = [0];
+  var sidec = [$('h0')];
+  $('h1').each(function() {
+    list.push($(this).get(0).offsetTop  - 15);
+  });
+  $('.jump').each(function() {
+    sidec.push($(this));
+  });
+
+  $(window).scroll(function() {
+    console.log(list, $(window).scrollTop());
+    $('.jump').each(function() {
+      $(this).removeClass('active');
+    });
+    for(var i = 0; i < list.length; i++) {
+      if(list[i] > $(window).scrollTop()) {
+        sidec[i].addClass('active');
+        break;
+      }
+    }
+  })
+}
