3636e4a8953bfb6e1657fb0b3f99de329047b9a5
nyukke415 <nyukke415@gmail.com>
2019-07-01 01:30:18 +0900

    年齢計算のバグを修正.

--- js/calcAge.js
+++ js/calcAge.js
@@ -1,13 +1,15 @@
-/* last updated:<2019/06/30/Sun 23:48:41 from:gale-ThinkPad-T480s> */
+/* last updated:<2019/07/01/Mon 01:29:27 from:gale-ThinkPad-T480s> */
 
 function calcAge() {
-  const birthday = new Date(1996, 4-1, 15);
+  const birthdate = new Date(1996, 4-1, 15);
   const today = new Date();
-  var age = today.getFullYear() - birthday.getFullYear();
-  var age_m = today.getMonth() - birthday.getMonth();
-  var age_d = today.getDate() - birthday.getDate();
+  var age = today.getFullYear() - birthdate.getFullYear();
+  var age_m = today.getMonth() - birthdate.getMonth();
+  var age_d = today.getDate() - birthdate.getDate();
+  console.log(age, age_m, age_d);
   if(age_d < 0) {
-    age_d = -1 + new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+    age_d += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+    age_m -= 1;
   }
   if(age_m < 0) {
     age_m += 12;
